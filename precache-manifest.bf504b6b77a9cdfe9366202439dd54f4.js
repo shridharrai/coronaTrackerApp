8856ae628814e3e7657fd54bb42cab4ee4a2765f
shridharrai <shridharrai3@gmail.com>
2020-04-19 14:01:24 +0530

    Updates

---
+++ precache-manifest.bf504b6b77a9cdfe9366202439dd54f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af18d8d2f93528110933c29dc760f506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "429b0d04f9e4bf645560",
+    "url": "/static/css/main.2d40c17e.chunk.css"
+  },
+  {
+    "revision": "a18a9f731acf4420a155",
+    "url": "/static/js/2.d57787bf.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.d57787bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429b0d04f9e4bf645560",
+    "url": "/static/js/main.f7ffbc35.chunk.js"
+  },
+  {
+    "revision": "ef5c423c1e5d1eb420e5",
+    "url": "/static/js/runtime-main.b717093d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
